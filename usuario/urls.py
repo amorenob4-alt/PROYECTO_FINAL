041cc6413b56989b4b478a0407f4ec63b79b2649
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-05 14:06:52 -0500

    Feat: Dashboard completado con graficos y proteccion de login

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.lista_usuarios, name="lista_usuarios"),
+
+    path('', views.lista_usuarios, name='lista_usuarios'),
+
+    path(
+        'crear/',
+        views.crear_usuario,
+        name='crear_usuario'
+    ),
+
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,12 +1,42 @@
-
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.contrib.auth.models import User
+from .forms import UsuarioForm
+
 
 @login_required
 def lista_usuarios(request):
-    usuarios = User.objects.all()
-    return render(request, "usuario/lista_usuarios.html", {
-        "usuarios": usuarios
-    })
 
+    usuarios = User.objects.all()
+
+    return render(
+        request,
+        "usuario/lista_usuarios.html",
+        {
+            "usuarios": usuarios
+        }
+    )
+
+
+@login_required
+def crear_usuario(request):
+
+    if request.method == "POST":
+
+        form = UsuarioForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("lista_usuarios")
+
+    else:
+
+        form = UsuarioForm()
+
+    return render(
+        request,
+        "usuario/crear_usuario.html",
+        {
+            "form": form
+        }
+    )
