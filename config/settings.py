70731a5caf7950705494584c0e9da5e6185d95c6
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-05 11:49:29 -0500

    Configurar Render

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&3y4=fj9@!6=zeqptjhg7#2pdyzmq$8l_c-3o*vz@2#$3nvrfm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =[".onrender.com",
+    "localhost",
+    "127.0.0.1",]
 
 
 # Application definition
@@ -81,7 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'laboratorio.db',
+        'NAME': BASE_DIR / 'laboratorio.db',
         'USER': 'root',
         'PASSWORD': 'zeusybaki3',
         'HOST': 'localhost',
@@ -125,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
