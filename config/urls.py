41f3af4cf88ff51da3b5415c08fd3b2161bdebb8
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-05 19:41:13 -0500

    Corregir rutas principales

--- config/urls.py
+++ config/urls.py
@@ -18,13 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-
 urlpatterns = [
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='inicio'),
+
     path('admin/', admin.site.urls),
-    path('', include('inventario.urls')),      # Dashboard
+    path('', include('inventario.urls')),
     path('prestamos/', include('prestamo.urls')),
     path('usuarios/', include('usuario.urls')),
+
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
-
