70b360dde795df512ba6782664d3569be35452d5
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-06 16:36:42 -0500

    Se agregó el dashboard

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -88,10 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'laboratorio.db',
-        'USER': 'root',
-        'PASSWORD': 'zeusybaki3',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        
     }
 }
 
