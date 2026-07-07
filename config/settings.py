39ff664a5520e7bf09eadc7765d0443788f2e2c5
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-06 20:25:30 -0500

    Cambiar base de datos a SQLite

--- config/settings.py
+++ config/settings.py
@@ -84,18 +84,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'laboratorio',
-        'USER': 'root',
-        'PASSWORD': 'zeusybaki3',
-        'HOST': 'localhost',
-        'PORT': '3307',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'laboratorio.db',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
