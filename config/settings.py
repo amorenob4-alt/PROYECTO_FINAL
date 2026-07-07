ed46fdda0d953b484a3dd628258fb436b4452c0e
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-06 19:04:46 -0500

    Se modifico

--- config/settings.py
+++ config/settings.py
@@ -87,12 +87,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'laboratorio.db',
-        
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'laboratorio',
+        'USER': 'root',
+        'PASSWORD': 'zeusybaki3',
+        'HOST': 'localhost',
+        'PORT': '3307',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
