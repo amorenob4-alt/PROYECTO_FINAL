b5a188144a296e2d49441bcba959e5330076e6ad
amorenob4-alt <amorenob4@unemi.edu.ec>
2026-07-07 14:45:30 -0500

    Corregir configuración de archivos estáticos

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STORAGES = {
     "staticfiles": {
